bb557470f0797e7cb7b3921434fa8f95205a82da
suvanna <suviey1997@gmail.com>
2022-05-09 00:17:26 -0400

    commit

---
+++ Java/Day13.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(1000, 1000);
+    background(142, 137, 150);
+    angleMode(DEGREES);
+}
+
+function draw() {
+
+function mousePressed(){
+    saveCanvas("Day13", "png")
+}  
+}
